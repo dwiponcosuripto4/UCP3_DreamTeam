dfc4843fe7f4c966c4aead3a6b65be6bbed981ea
Muhammad Davin Widyatmaka <davin.new58@gmail.com>
2024-12-29 23:04:54 +0700

    add ball script( move bounce ),add physic material

---
+++ UCP3_DreamTeam/Assets/Script/ball.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    public float speed = 10.0f; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        float[] rayy = new float[2];
+        rayy[0] = -5.0f;
+        rayy[1] = 5.0f;
+        int x = (int)Random.Range(0,1.4f);
+        int y = (int)Random.RandomRange(-4.5f,4.5f);
+        GetComponent<Rigidbody>().velocity = new Vector3(x,0,y) * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
