664e9950205307a8ace4f36c83d31ec65754e41f
Muhammad Davin Widyatmaka <davin.new58@gmail.com>
2025-01-14 15:35:22 +0700

    edit obstacle and logic problem

---
+++ UCP3_DreamTeam/Assets/Script/ObstacleComp.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleComp : MonoBehaviour
+{
+    private float speed = 6.0f;
+    private float range = 5.0f;
+
+    private float startZ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startZ = transform.localPosition.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Hitung posisi baru menggunakan sinusoidal untuk bolak-balik
+        float newZ = startZ + Mathf.Sin(Time.time * speed) * range;
+        transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, newZ);
+    }
+}
--- UCP3_DreamTeam/Assets/Script/ball.cs
+++ UCP3_DreamTeam/Assets/Script/ball.cs
@@ -7,73 +7,61 @@ using Unity.VisualScripting;
 
 public class ball : MonoBehaviour
 {
-    private float speed = 4.0f;
+    private float speed = 5.0f;
     public GameObject winnerp1, winnerp2;
     public TMP_Text skorp1_tampil, skorp2_tampil;
-    int skorp1 = 0,skorp2 = 0;
+    private int skorp1 = 0,skorp2 = 0;
     // Start is called before the first frame update
     void Start()
     {
-        float[] rayy = new float[2];
-        rayy[0] = -6.0f;
-        rayy[1] = 6.0f;
-        int x = (int)Random.Range(0,1.4f);
-        int y = (int)Random.Range(-4.5f,4.5f);
-        GetComponent<Rigidbody>().velocity = new Vector3(x,0,y) * speed;
+        float x = Random.Range(0, 2) == 0 ? -1 : 1;
+        float y = Random.Range(-1.0f, 1.0f);
+        GetComponent<Rigidbody>().velocity = new Vector3(x, 0, y).normalized * speed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        skorp1_tampil.text = skorp1.ToString();
+        skorp2_tampil.text = skorp2.ToString();
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.name == "Wall p1")
-        {
-            if (skorp1 <= 30)
-            {
-                skorp1 = 0;
-                winner(winnerp2);
-            }
-            else
-            {
-                skorp1 -= 30;
-            }
-        }
-        if(collision.gameObject.name == "Wall p2")
-        {
-            if (skorp2 <= 30)
-            {
-                skorp2 = 0;
-                winner(winnerp1);
-            }
-            else
-            {
-                skorp2 -= 30;
-            }
-        }
-        if(collision.gameObject.name == "player 1")
+        if (collision.gameObject.name == "Wall p2")
         {
             skorp1 += 10;
-            
+            CheckWinner(winnerp1, skorp1);
         }
-        if(collision.gameObject.name == "player 2")
+        // Jika bola menabrak dinding pemain 1 (poin untuk pemain 2)
+        else if (collision.gameObject.name == "Wall p1")
+        {
+            skorp2 += 10;
+            CheckWinner(winnerp2, skorp2);
+        }
+        // Jika bola mengenai pemain 2
+        else if (collision.gameObject.name == "player 2")
+        {
+            skorp1 += 10;
+        }
+        // Jika bola mengenai pemain 1
+        else if (collision.gameObject.name == "player 1")
         {
             skorp2 += 10;
         }
-        skorp1_tampil.text = skorp1.ToString();
-        skorp2_tampil.text = skorp2.ToString();
+
+        // Mainkan suara setelah setiap tabrakan
         GetComponent<AudioSource>().Play();
     }
 
-    public void winner(GameObject obj)
+    private void CheckWinner(GameObject winnerObj, int score)
     {
-        obj.SetActive(true);
-        Time.timeScale = 0f;
+        if (score >= 30)
+        {
+            winnerObj.SetActive(true);
+            Time.timeScale = 0f;
+        }
     }
-
     public void restart()
     {
         Time.timeScale = 1.0f;
