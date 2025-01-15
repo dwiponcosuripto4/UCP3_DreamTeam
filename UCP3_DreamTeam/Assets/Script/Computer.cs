c767ec5487c61ab2debadcec249477e6ec6a8de4
dwiponcosuripto4 <115076093+dwiponcosuripto4@users.noreply.github.com>
2025-01-15 11:00:23 +0700

    fix computer

--- UCP3_DreamTeam/Assets/Script/Computer.cs
+++ UCP3_DreamTeam/Assets/Script/Computer.cs
@@ -4,20 +4,52 @@ using UnityEngine;
 
 public class Computer : MonoBehaviour
 {
-    public Transform ball;
+    public Transform ball; 
+    public float speed = 5f; 
+    public float decisionInterval = 2f;
+
+    private bool followBall = true; 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(ChangeBehaviorRoutine());
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (ball.localPosition.z <= 6.0f && ball.localPosition.z >= -6.0f)
+        if (followBall)
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y, ball.position.z);
+            if (ball.localPosition.z <= 6.0f && ball.localPosition.z >= -6.0f)
+            {
+                float step = speed * Time.deltaTime; 
+                Vector3 targetPosition = new Vector3(transform.position.x, transform.position.y, ball.position.z);
+                transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
+            }
         }
+        else
+        {
+            RandomIdleMovement();
+        }
+    }
+
+    private IEnumerator ChangeBehaviorRoutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(decisionInterval);
+
+            followBall = Random.value > 0.5f; 
+        }
+    }
+
+    // Fungsi untuk gerakan idle saat tidak mengikuti bola
+    private void RandomIdleMovement()
+    {
+        float step = speed * 0.5f * Time.deltaTime; 
+        float randomZ = Mathf.PingPong(Time.time, 12f) - 6f; 
+        Vector3 idlePosition = new Vector3(transform.position.x, transform.position.y, randomZ);
+        transform.position = Vector3.MoveTowards(transform.position, idlePosition, step);
     }
 }
